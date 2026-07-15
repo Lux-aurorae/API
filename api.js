30a2d0163d846803928964ca62f9ecd018f053ce
Lux-aurorae <sherlock216@naver.com>
2026-07-15 22:15:03 +0900

    API-commit

--- api.js
+++ api.js
@@ -1,13 +1,16 @@
-let news=[];
-const getLatestNews=async()=>{
-    let url=new URL('https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?q=아이유=${PAGE_SIZE}');
-const response = await fetch(url);
-//URL 호출 : 인터넷세상에서 데이터(자료를)불러오는 것
-const data=await response.json();
-news = data.articles;
-console.log("dddd",news);
-//console.log("dddd",data.articles);
-//let news=data.articles;
+let news = [];
+const PAGE_SIZE = 10;  
+
+const getLatestNews = async () => {
+  let url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?q=아이유&pageSize=${PAGE_SIZE}`
+  );
+
+  const response = await fetch(url); 
+  const data = await response.json();  
+
+  news = data.articles;             
+  console.log("뉴스 데이터:", news);
 };
 
 getLatestNews();
