d77e33091c3ce75e2bc7193d554847a4cc6b11c2
Lux-aurorae <sherlock216@naver.com>
2026-07-15 22:05:04 +0900

    api-commit

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
-const API_KEY='9053e19d896a443fa62e864f6ad707f7';
 let news=[];
 const getLatestNews=async()=>{
-    let url=new URL('https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}');
+    let url=new URL('https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${PAGE_SIZE}');
 const response = await fetch(url);
 //URL 호출 : 인터넷세상에서 데이터(자료를)불러오는 것
 const data=await response.json();
